8b1a110b76fd58a2ab1afd9d7c24a5641a3fc47e
OmarEmadSaad <oemad3987@gmail.com>
2025-04-21 17:51:28 +0200

    MoviesApp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">App</h1>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
